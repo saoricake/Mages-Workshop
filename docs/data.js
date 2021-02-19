b443b2ced61bdde0ea3a26363073109b21554d5c
saori m <trhvmn.abcn@gmail.com>
2021-02-19 18:46:54 -0300

    Add changes from previous fork

---
+++ docs/data.js
@@ -0,0 +1,11 @@
+export function filterData(data, condition) {
+	console.log(data, condition);
+}
+
+export function sortData(data, sortBy, sortOrder) {
+	console.log(data, sortBy, sortOrder);
+}
+
+export function computeStats(data) {
+	console.log(data);
+}
---
+++ docs/main.js
@@ -0,0 +1,3 @@
+import {filterData, sortData, computeStats} from './data.js';
+
+console.log(filterData, sortData, computeStats);
