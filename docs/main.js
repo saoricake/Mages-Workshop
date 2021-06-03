de78080e2f6347a6e36e8a078bb8dc3aa72c86f2
saori m <trhvmn.abcn@gmail.com>
2021-06-03 17:51:37 -0300

    Add preliminary pagination functionality

--- docs/main.js
+++ docs/main.js
@@ -1,147 +1,172 @@
 const form = document.forms[0];
+const formControls = form.elements;
+const formValuesStorage = window.sessionStorage;
 const table = document.querySelector("table"),
 	tableHeaders = table.tHead.rows[0].cells,
 	tableBody = table.tBodies[0],
 	tableRows = table.tBodies[0].rows;
 const database = fetch("./data/nice_equip.json").then(r => r.json());
 
-function filterData() {
-	const formData = new FormData(form);
-	const inputName = formData.get("name");
-	const inputRarity = (formData.getAll("rarity")).join("|");
-	const inputHpBase = formData.get("hpBase");
-	const inputHpMax = formData.get("hpMax");
-	const inputAtkBase = formData.get("atkBase");
-	const inputAtkMax = formData.get("atkMax");
+function updateSearchParams() {
+	const defaultSearchParams = new URLSearchParams([
+		["name", ""],
+		["rarity", "1|2|3|4|5"],
+		["min-hp", "\d{0,4}"],
+		["max-hp", "\d{0,4}"],
+		["min-atk", "\d{0,4}"],
+		["max-atk", "\d{0,4}"],
+		["sort-by", "collectionNo"],
+		["sort-order", "asc"],
+		["page", 1]
+	]);
+	const newSearchParams = new URLSearchParams(window.location.search);
+	newSearchParams.set("rarity", (newSearchParams.getAll("rarity")).join("|"));
+	const updatedSearchParams = new URLSearchParams();
 
-	const rowsArray = Array.from(tableRows);
-	
-	rowsArray.forEach(row => {
-		if (
-			RegExp(inputName, "i").test(row.cells[1].textContent) &&
-			RegExp(inputRarity).test(row.cells[2].textContent) &&
-			RegExp(String.raw`\b${inputHpBase}\b`).test(row.cells[3].textContent) &&
-			RegExp(String.raw`\b${inputHpMax}\b`).test(row.cells[4].textContent) &&
-			RegExp(String.raw`\b${inputAtkBase}\b`).test(row.cells[5].textContent) &&
-			RegExp(String.raw`\b${inputAtkMax}\b`).test(row.cells[6].textContent)
-		) {
-			row.classList.remove("hidden");
-		} else {
-			row.classList.add("hidden");
-		}
+	defaultSearchParams.forEach((value, key) => {
+		updatedSearchParams.set(key, value);
+	});
+	newSearchParams.forEach((value, key) => {
+		updatedSearchParams.set(key, value);
+	});
+	return updatedSearchParams;
+}
+
+function loadFormValues() {
+	(document.querySelectorAll(`input[type="text"]`)).forEach(textInput => {
+		if (formValuesStorage.getItem(textInput.name)) textInput.value = formValuesStorage.getItem(textInput.name);
 	});
 
-	const visibleRows = rowsArray.filter(row => row.classList.contains("hidden"));
+	(document.querySelectorAll(`input[type="checkbox"]`)).forEach(checkboxInput => {
+		if (formValuesStorage.getItem(checkboxInput.id)) checkboxInput.checked = true;
+	});
 
-	visibleRows.forEach((row, rowIndex) => {
-		if ((rowIndex & 1) === 1) {
-			row.classList.add("zebra");
-		} else {
-			row.classList.remove("zebra");
+	(document.querySelectorAll("select")).forEach(selectInput => {
+		if (formValuesStorage.getItem(selectInput.name)) selectInput.options[formValuesStorage.getItem(selectInput.name)].selected = true;
+	});
+
+	formValuesStorage.clear();
+}
+
+function saveFormValues() {
+	(document.querySelectorAll(`input[type="text"]`)).forEach(textInput => {
+		if (textInput.value === textInput.defaultValue) textInput.disabled = true;
+		else formValuesStorage.setItem(textInput.name, textInput.value);
+	});
+
+	(document.querySelectorAll(`input[type="checkbox"]`)).forEach(checkboxInput => {
+		if (checkboxInput.checked === false) checkboxInput.disabled = true;
+		else formValuesStorage.setItem(checkboxInput.id, true);
+	});
+
+	(document.querySelectorAll("select")).forEach(selectInput => {
+		if (selectInput.options[0].selected === true) selectInput.disabled = true;
+		else formValuesStorage.setItem(selectInput.name, selectInput.selectedIndex);
+	});
+
+	// if (formControls["page"].value === 1) formControls["page"].disabled = true;
+}
+
+async function filterDatabase(database, searchParams) {
+	const filteredDatabase = (await database).filter(craftEssence => {
+		return RegExp(searchParams.get("name"), "i").test(craftEssence["name"]) &&
+			RegExp(searchParams.get("rarity")).test(craftEssence["rarity"]) &&
+			RegExp(String.raw`\b${searchParams.get("min-hp")}\b`).test(craftEssence["hpBase"]) &&
+			RegExp(String.raw`\b${searchParams.get("max-hp")}\b`).test(craftEssence["hpMax"]) &&
+			RegExp(String.raw`\b${searchParams.get("min-atk")}\b`).test(craftEssence["atkBase"]) &&
+			RegExp(String.raw`\b${searchParams.get("max-atk")}\b`).test(craftEssence["atkMax"]);
+	});
+
+	return filteredDatabase.filter((craftEssence, index) => {
+		return (index >= (searchParams.get("page")-1)*50) && (index < (searchParams.get("page")*50));
+	});
+}
+
+async function sortDatabase(database, searchParams) {
+	const sortBy = searchParams.get("sort-by"),
+		sortOrder = searchParams.get("sort-order"),
+		sortParams = new Intl.Collator("en", {
+			sensitivity: "base",
+			ignorePunctuation: "true",
+			numeric: "true",
+			caseFirst: "upper"
+		});
+
+	return (await database).sort((firstCraftEssence, secondCraftEssence) => {
+		if (sortOrder == "desc") {
+			return sortParams.compare(secondCraftEssence[sortBy], firstCraftEssence[sortBy]);
+		} else if (sortOrder == "asc") {
+			return sortParams.compare(firstCraftEssence[sortBy], secondCraftEssence[sortBy]);
 		}
 	});
 }
 
-function sortData(sortBasisHeader) {
-	const headersArray = Array.from(tableHeaders),
-		rowsArray = Array.from(tableRows);
-	const sortBasisClassList = sortBasisHeader.classList;
-	const sortParams = new Intl.Collator("en", {
-		sensitivity: "base",
-		ignorePunctuation: "true",
-		numeric: "true",
-		caseFirst: "upper"
-	});
+loadFormValues();
 
-	if (sortBasisClassList.contains("sortBasis") === false) {
-		headersArray.forEach(allHeaders => allHeaders.classList.remove("sortBasis", "sortDesc"));
-		sortBasisClassList.add("sortBasis");
-	} else {
-		sortBasisClassList.toggle("sortDesc");
-	}
-	rowsArray.sort((firstRow, secondRow) => {
-		const firstRowSortValue = firstRow.cells[sortBasisHeader.cellIndex].textContent;
-		const secondRowSortValue = secondRow.cells[sortBasisHeader.cellIndex].textContent;
-		
-		if (sortBasisClassList.contains("sortDesc")) {
-			return sortParams.compare(secondRowSortValue, firstRowSortValue);
-		} else {
-			return sortParams.compare(firstRowSortValue, secondRowSortValue);
-		}
-	});
-	rowsArray.forEach((row, rowIndex) => {
-		tableBody.appendChild(row);
-		if ((rowIndex & 1) === 1) {
-			row.classList.add("zebra");
-		} else {
-			row.classList.remove("zebra");
-		}
-	});
-}
+(async function printTable() {
+	const searchParams = updateSearchParams();
+	const searchedDatabase = await filterDatabase(sortDatabase(database, searchParams), searchParams);
 
-(async function initializeData() {
-	const inputStatsArray = [
-		[form.elements["hpBase"], []],
-		[form.elements["hpMax"], []],
-		[form.elements["atkBase"], []],
-		[form.elements["atkMax"], []]
-	];
-
-	(await database).forEach(ce => {
-		const params = [
-			ce["collectionNo"],
-			ce["extraAssets"]["equipFace"]["equip"][ce["id"]],
-			ce["name"],
-			ce["rarity"],
-			ce["hpBase"],
-			ce["hpMax"],
-			ce["atkBase"],
-			ce["atkMax"],
-			ce["skills"][0]["icon"],
-			ce["skills"][0]["detail"] + (ce["skills"][1] ? `\n${ce["skills"][1]["detail"]}` : "")
+	searchedDatabase.forEach(craftEssence => {
+		const craftEssenceParams = [
+			craftEssence["collectionNo"],
+			craftEssence["extraAssets"]["equipFace"]["equip"][craftEssence["id"]],
+			craftEssence["name"],
+			craftEssence["rarity"],
+			craftEssence["hpBase"],
+			craftEssence["hpMax"],
+			craftEssence["atkBase"],
+			craftEssence["atkMax"],
+			craftEssence["skills"][0]["icon"],
+			craftEssence["skills"][0]["detail"] + (craftEssence["skills"][1] ? `\n${craftEssence["skills"][1]["detail"]}` : "")
 		];
 
-		if (inputStatsArray[0][1].includes(params[4]) === false) inputStatsArray[0][1].push(params[4]);
-		if (inputStatsArray[1][1].includes(params[5]) === false) inputStatsArray[1][1].push(params[5]);
-		if (inputStatsArray[2][1].includes(params[6]) === false) inputStatsArray[2][1].push(params[6]);
-		if (inputStatsArray[3][1].includes(params[7]) === false) inputStatsArray[3][1].push(params[7]);
-
-		const newTableRow =
-		`<tr>
-			<td>${params[0]}</td>
-			<td><img src=${params[1]} loading="lazy" height=30><span>${params[2]}</span></td>
-			<td>${params[3]}</td>
-			<td>${params[4]}</td>
-			<td>${params[5]}</td>
-			<td>${params[6]}</td>
-			<td>${params[7]}</td>
-			<td><img src=${params[8]} alt="${params[9]}" title="${params[9]}" loading="lazy" height=30></td>
-		</tr>`;
-
-		tableBody.insertAdjacentHTML("beforeend", newTableRow);
-	});
-	sortData(tableHeaders[0]);
-
-	inputStatsArray.forEach(eles => {
-		eles[1].sort((a, b) => a - b);
-		eles[1].forEach(options => {
-			eles[0].insertAdjacentHTML("beforeend", `<option value="${options}">${options}</option>`)
-		});
+		tableBody.insertAdjacentHTML("beforeend",
+			`<tr>
+				<td>${craftEssenceParams[0]}</td>
+				<td>
+					<img
+						src=${craftEssenceParams[1]}
+						height=30>
+					<span>${craftEssenceParams[2]}</span>
+				</td>
+				<td>${craftEssenceParams[3]}</td>
+				<td>${craftEssenceParams[4]}</td>
+				<td>${craftEssenceParams[5]}</td>
+				<td>${craftEssenceParams[6]}</td>
+				<td>${craftEssenceParams[7]}</td>
+				<td>
+					<img
+						src=${craftEssenceParams[8]}
+						alt="${craftEssenceParams[9]}"
+						title="${craftEssenceParams[9]}"
+						height=30>
+				</td>
+			</tr>`
+		);
 	});
 })();
 
-(function initializeEventListeners() {
-	const formControls = Array.from(form.elements);
-	const headersArray = Array.from(tableHeaders);
-	headersArray.pop();
+form.onsubmit = saveFormValues;
 
-	function listenerFilter() {filterData();}
-	function listenerSort(event) {sortData(event.target);}
+/* (async function findUniqueStats() {
+	const inputStatsArray = [
+		[form.elements["minHpInput"], []],
+		[form.elements["maxHpInput"], []],
+		[form.elements["minAtkInput"], []],
+		[form.elements["maxAtkInput"], []]
+	];
 
-	form.addEventListener("submit", event => event.preventDefault());
-	formControls.forEach(formControl => formControl.addEventListener("input", listenerFilter));
-	headersArray.forEach(header => header.addEventListener("click", listenerSort));
-	headersArray.forEach(header => header.addEventListener("keydown", event => {
-		if (event.key === "Enter") listenerSort(event);
-	}));
-})();
+	(await database).forEach(craftEssence => {
+		if (!inputStatsArray[0][1].includes(craftEssence["hpBase"])) inputStatsArray[0][1].push(craftEssence["hpBase"]);
+		if (!inputStatsArray[1][1].includes(craftEssence["hpMax"])) inputStatsArray[1][1].push(craftEssence["hpMax"]);
+		if (!inputStatsArray[2][1].includes(craftEssence["atkBase"])) inputStatsArray[2][1].push(craftEssence["atkBase"]);
+		if (!inputStatsArray[3][1].includes(craftEssence["atkMax"])) inputStatsArray[3][1].push(craftEssence["atkMax"]);
+	});
+
+	inputStatsArray.forEach(elements => {
+		elements[1].sort((a, b) => a - b);
+		console.log(elements[0]);
+		console.log(elements[1]);
+	});
+})(); */
